3cbdd43683378c07d95528f914d12fd51e0a2e4c
irfan-deva <irfandeva@gmail.com>
2023-07-14 10:45:09 +0530

    added product and user pages

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -9,7 +9,7 @@ const Menu = () => {
         <div className="item" key={item.id}>
           <span className="title">{item.title}</span>
           {item.listItems.map((listItem) => (
-            <Link className="listItem" to={"/"} key={listItem.id}>
+            <Link className="listItem" to={listItem.url} key={listItem.id}>
               <img src={listItem.icon} alt="" />
               <span className="listItemTitle">{listItem.title}</span>
             </Link>
--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -79,6 +79,8 @@ const Single = () => {
             </div>
           </div>
         </div>
+        <hr />
+
         <div className="chart">
           <ResponsiveContainer width="100%" height="100%">
             <LineChart
